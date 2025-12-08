e9b177e8e541370eda3bf5f1907044ee60900549
NithyaPrakash18 <NithyaPrakash18@users.noreply.github.com>
2025-12-08 15:42:31 +0530

    day06: Add react-router-dom, 404 page and useState practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
 import "./App.css";
-import FlexBoxComponent from "./flex-box/flex-box-component";
-import FlexWrapComponent from "./flex-wrap/flex-wrap-component";
+
+import { Routes, Route } from "react-router-dom";
 import HelloWorldComponent from "./hello-world/hello-world-component";
-import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
 import OurPromisesComponent from "./our-promises/our-promises";
-import PositionExampleComponent from "./position-example/position-example-component";
-import PostionComponent from "./positioning/position-component";
-import PropsPracticeTwo from "./props-practice-2/props-practice-2";
-import PropsPractice from "./props-practice/props-practice";
-import SymbolOfExcellenceComponent from "./symbol-of-excellence/symbol-of-execllence-component";
 import TrustedbyComponent from "./trusted-by/trusted-by-component";
+import SymbolOfExcellenceComponent from "./symbol-of-excellence/symbol-of-execllence-component";
+import FourOFourComponent from "./404-component/404-component";
+import UseStateComponent from "./usestate-1st/use-state-component";
 
 function App() {
-  const arr1 = [
-    { name: "sai", mobile: "1234" },
-    { name: "ragu", mobile: "5678" },
-  ];
   return (
     <div>
-      {/* <HelloWorldComponent /> */}
-      {/* <FlexBoxComponent /> */}
-      {/* <LeadersTomorrowComponent /> */}
-      {/* <SymbolOfExcellenceComponent /> */}
-      {/* <PostionComponent /> */}
-      {/* <TrustedbyComponent /> */}
-      {/* <FlexWrapComponent /> */}
-      {/* <OurPromisesComponent /> */}
-      {/* <PositionExampleComponent /> */}
-      {/* <PropsPractice name="Sai" mobile="14234423243" /> */}
-      <PropsPracticeTwo data={arr1} />
+      <Routes>
+        <Route path="/eshwar" element={<HelloWorldComponent />} />
+        <Route path="/our-promise" element={<OurPromisesComponent />} />
+        <Route path="/" element={<TrustedbyComponent />} />
+        <Route path="/leetcode/:id" element={<SymbolOfExcellenceComponent />} />
+        <Route path="*" element={<FourOFourComponent />} />
+        <Route path="/use-state-practice" element={<UseStateComponent />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/usestate-1st/use-state-component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+export const UseStateStyle = styled.div``;
+
+const UseStateComponent = () => {
+  return (
+    <UseStateStyle>
+      <h1>UseState Practice</h1>
+    </UseStateStyle>
+  );
+};
+
+export default UseStateComponent;
